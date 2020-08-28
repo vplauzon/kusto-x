9a28cf1f66a98106df9d4e841725904511b6068b
vplauzon <vplauzon@vplauzon.com>
2020-08-28 19:53:31 -0400

    .

---
+++ components/api-status/api-status-solution/ApiStatus/Controllers/RootController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiStatus.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        private readonly ILogger<RootController> _logger;
+
+        public RootController(ILogger<RootController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new ObjectResult(new
+            {
+                Version = ApiVersion.FullVersion,
+                Status = "Ready"
+            });
+        }
+    }
+}
