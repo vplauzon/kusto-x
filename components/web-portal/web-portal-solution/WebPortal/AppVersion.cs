ffba3a11bce8a07793b42c516b1881c125488a1b
vplauzon <vplauzon@vplauzon.com>
2020-08-29 10:45:27 -0400

    .

---
+++ components/web-portal/web-portal-solution/WebPortal/AppVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPortal
+{
+    public class AppVersion
+    {
+        public static string FullVersion { get { return "FULL_VERSION"; } }
+    }
+}
