1570bf0dff54e3024e659f6472e3a15c84471311
vplauzon <vplauzon@vplauzon.com>
2020-09-16 19:50:13 -0400

    .

---
+++ components/api-planning/api-planning-solution/ApiPlanning/Controllers/RequestController.cs
@@ -0,0 +1,34 @@
+﻿using IntegrationPlanning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ApiPlanning.Controllers
+{
+    [ApiController]
+    [Route("/v1")]
+    public class RequestController : ControllerBase
+    {
+        private readonly ILogger<RootController> _logger;
+
+        public RequestController(ILogger<RootController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Consumes("text/plain")]
+        public async Task<string> PostAsync()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var text = await reader.ReadToEndAsync();
+                var planner = new IntegratedPlanner();
+                var output = await planner.PushRequestAsync(text);
+
+                return "42";
+            }
+        }
+    }
+}
---
+++ components/api-planning/api-planning-solution/IntegrationPlanning/IntegratedPlanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPlanning
+{
+    public class IntegratedPlanner
+    {
+        public async Task<string> PushRequestAsync(string text)
+        {
+            await Task.CompletedTask;
+
+            throw new NotImplementedException();
+        }
+    }
+}
