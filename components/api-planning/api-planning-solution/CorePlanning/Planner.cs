185df0cf381fc5e1f30a7f31b0dd723e1010662f
vplauzon <vplauzon@vplauzon.com>
2020-09-16 20:20:45 -0400

    .

---
+++ components/api-planning/api-planning-solution/CorePlanning/Planner.cs
@@ -0,0 +1,43 @@
+﻿using PasApiClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePlanning
+{
+    public class Planner
+    {
+        private static readonly string GRAMMAR = GetGrammar();
+
+        public async Task ProcessRequestAsync(string text)
+        {
+            await ParseRequestAsync(text);
+        }
+
+        private Task ParseRequestAsync(string text)
+        {
+            var client = ParserClient.Create();
+
+            client.SingleParseAsync(GRAMMAR, text);
+
+            throw new NotImplementedException();
+        }
+
+        private static string GetGrammar()
+        {
+            var assembly = typeof(Planner).GetTypeInfo().Assembly;
+            var fullResourceName = "CorePlanning.dataflow-grammar.txt";
+
+            using (var stream = assembly.GetManifestResourceStream(fullResourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var text = reader.ReadToEnd();
+
+                return text;
+            }
+        }
+    }
+}
