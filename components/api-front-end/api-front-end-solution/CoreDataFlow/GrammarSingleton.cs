f7115f36378bec9ca4e733c49d97106bd07bf969
vplauzon <vplauzon@vplauzon.com>
2020-09-19 21:28:44 -0400

    Refactor into GrammarSingleton

---
+++ components/api-front-end/api-front-end-solution/CoreDataFlow/GrammarSingleton.cs
@@ -0,0 +1,29 @@
+﻿using PasLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CoreDataFlow
+{
+    public static class GrammarSingleton
+    {
+        public static Grammar Instance { get; } = LoadGrammar();
+
+        private static Grammar LoadGrammar()
+        {
+            var assembly = typeof(Planner).GetTypeInfo().Assembly;
+            var fullResourceName = typeof(GrammarSingleton).Name + ".dataflow-grammar.txt";
+
+            using (var stream = assembly.GetManifestResourceStream(fullResourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var text = reader.ReadToEnd();
+                var grammar = MetaGrammar.ParseGrammar(text);
+
+                return grammar;
+            }
+        }
+    }
+}
--- components/api-front-end/api-front-end-solution/CoreDataFlow/Planner.cs
+++ components/api-front-end/api-front-end-solution/CoreDataFlow/Planner.cs
@@ -10,8 +10,6 @@ namespace CoreDataFlow
 {
     public class Planner
     {
-        private static readonly Grammar GRAMMAR = GetGrammar();
-
         public async Task ProcessRequestAsync(string text)
         {
             await ParseRequestAsync(text);
@@ -19,24 +17,9 @@ namespace CoreDataFlow
 
         private Task ParseRequestAsync(string text)
         {
-            GRAMMAR.Match("main", text);
+            GrammarSingleton.Instance.Match("main", text);
 
             throw new NotImplementedException();
-        }
-
-        private static Grammar GetGrammar()
-        {
-            var assembly = typeof(Planner).GetTypeInfo().Assembly;
-            var fullResourceName = "CorePlanning.dataflow-grammar.txt";
-
-            using (var stream = assembly.GetManifestResourceStream(fullResourceName))
-            using (var reader = new StreamReader(stream))
-            {
-                var text = reader.ReadToEnd();
-                var grammar = MetaGrammar.ParseGrammar(text);
-
-                return grammar;
-            }
         }
     }
 }
