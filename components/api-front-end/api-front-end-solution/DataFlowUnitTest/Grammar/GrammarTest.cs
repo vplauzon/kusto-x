223b102704116e3ab97dc7b672befb237e585368
vplauzon <vplauzon@vplauzon.com>
2020-09-22 09:14:27 -0400

    Add concurrent + sequence

--- components/api-front-end/api-front-end-solution/DataFlowUnitTest/Grammar/GrammarTest.cs
+++ components/api-front-end/api-front-end-solution/DataFlowUnitTest/Grammar/GrammarTest.cs
@@ -10,9 +10,43 @@ namespace DataFlowUnitTest.Grammar
         public void Empty()
         {
             var test = @"dataflow
-{
-}";
+            {
+            }";
+         
+            TestText(test);
+        }
+
+        [Fact]
+        public void EmptySequence()
+        {
+            var test = @"dataflow
+            {
+                sequence
+                {
+                }
+            }";
+
+            TestText(test);
+        }
+
+        [Fact]
+        public void EmptyConcurrent()
+        {
+            var test = @"dataflow
+            {
+                concurrent
+                {
+                }
+            }";
+
+            TestText(test);
+        }
+
+        private static void TestText(string test)
+        {
             var match = GrammarSingleton.Instance.Match("main", test);
+
+            Assert.NotNull(match);
         }
     }
 }
