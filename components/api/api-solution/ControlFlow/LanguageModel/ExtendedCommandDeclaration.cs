65138ec2e907de35f0373cb7ac2af7dbe36f3f7e
vplauzon <vplauzon@vplauzon.com>
2021-02-10 14:22:12 -0500

    Add the extended command concept

---
+++ components/api/api-solution/ControlFlow/LanguageModel/ExtendedCommandDeclaration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlFlow.LanguageModel
+{
+    public class ExtendedCommandDeclaration
+    {
+        public ControlFlowDeclaration? AdHocControlFlow { get; set; }
+    }
+}
