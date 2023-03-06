239c6853fca211d14228c4b942aa62cc932c7fa4
vplauzon <vincent_lauzon@yahoo.com.au>
2023-03-06 09:46:01 -0500

    .

--- code/KustoX/Kustox.UnitTests/ForEachTest.cs
+++ code/KustoX/Kustox.UnitTests/ForEachTest.cs
@@ -23,5 +23,30 @@ namespace Kustox.UnitTests
             Assert.Equal("names", controlFlow.RootSequence.Blocks.First().ForEach!.Enumerator);
             Assert.Empty(controlFlow.RootSequence.Blocks.First().ForEach!.Sequence.Blocks);
         }
+
+        [Fact]
+        public void WithOneQueryForEach()
+        {
+            var script = @"@control-flow{
+    @foreach(n in names){
+        print 2
+    }
+}";
+            var controlFlow = new KustoxCompiler().CompileScript(script);
+
+            Assert.NotNull(controlFlow);
+            Assert.Single(controlFlow.RootSequence.Blocks);
+
+            //  For each
+            Assert.Null(controlFlow.RootSequence.Blocks.First().Capture);
+            Assert.NotNull(controlFlow.RootSequence.Blocks.First().ForEach);
+            Assert.Equal("n", controlFlow.RootSequence.Blocks.First().ForEach!.Cursor);
+            Assert.Equal("names", controlFlow.RootSequence.Blocks.First().ForEach!.Enumerator);
+
+            var subBlocks = controlFlow.RootSequence.Blocks.First().ForEach!.Sequence.Blocks;
+
+            Assert.Single(subBlocks);
+            Assert.NotNull(subBlocks.First().Query);
+        }
     }
 }
