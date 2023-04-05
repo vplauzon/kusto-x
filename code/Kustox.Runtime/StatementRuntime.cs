6bf7a920a833ab1c98291a9f75689d78b29336d5
vplauzon <vincent_lauzon@yahoo.com.au>
2023-04-05 18:25:37 -0400

    Add statement runtime

---
+++ code/Kustox.Runtime/StatementRuntime.cs
@@ -0,0 +1,17 @@
+﻿using Kustox.Runtime.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kustox.Runtime
+{
+    public class StatementRuntime
+    {
+        public Task<TableResult> RunStatementAsync(string statementText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
