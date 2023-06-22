69bb5b01d1058d7306ac4aa9e5c8640ab4a2934f
vplauzon <vincent_lauzon@yahoo.com.au>
2023-06-22 17:34:56 -0400

    .

--- code/Kustox.WebPortal/Program.cs
+++ code/Kustox.WebPortal/Program.cs
@@ -1,21 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Kustox.WebPortal
 {
-    app.UseExceptionHandler("/Error");
+    public static class Program
+    {
+        public static string Version
+        {
+            get
+            {
+                var assembly = typeof(Program).Assembly;
+                var version = assembly.GetName().Version;
+
+                return version!.ToString();
+            }
+        }
+
+        public static void Main(params string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
