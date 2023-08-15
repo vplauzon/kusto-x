5dce84d84d06c35ddc951d4a642952c9e3d0747a
vplauzon <vincent_lauzon@yahoo.com.au>
2023-08-15 09:34:08 -0400

    Remove API from deployment script

--- deployment/infra/deploy-infra.sh
+++ deployment/infra/deploy-infra.sh
@@ -6,18 +6,16 @@
 rg=$1
 env=$2
 workbenchVersion=$3
-apiVersion=$4
-tenantId=$5
-appId=$6
-appSecret=$7
+tenantId=$4
+appId=$5
+appSecret=$6
 
 echo "Resource group:  $rg"
 echo "Environment:  $env"
 echo "Workbench Version:  $workbenchVersion"
-echo "Workbench Version:  $apiVersion"
-echo "Workbench Version:  $tenantId"
-echo "Workbench Version:  $appId"
-echo "Workbench Version:  $appSecret"
+echo "Tenant ID:  $tenantId"
+echo "App ID:  $appId"
+echo "App Secret:  $appSecret"
 echo "Current directory:  $(pwd)"
 
 echo
@@ -26,5 +24,5 @@ echo "Deploying ARM template"
 az deployment group create -n "deploy-$(uuidgen)" -g $rg \
     --template-file infra.bicep \
     --parameters environment=$env \
-    workbenchVersion=$workbenchVersion apiVersion=$apiVersion \
+    workbenchVersion=$workbenchVersion \
     tenantId=$tenantId appId=$appId appSecret=$appSecret
