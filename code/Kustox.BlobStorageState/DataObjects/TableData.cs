182eb65d86974388bf8913567055a8e8d79b4c6e
vplauzon <vincent_lauzon@yahoo.com.au>
2023-08-20 10:28:14 -0400

    Fix int-tests

--- code/Kustox.BlobStorageState/DataObjects/TableData.cs
+++ code/Kustox.BlobStorageState/DataObjects/TableData.cs
@@ -4,15 +4,11 @@ using System.Data;
 using System.Data.SqlTypes;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Kustox.BlobStorageState.DataObjects
 {
     public class TableData
     {
-        private static readonly Newtonsoft.Json.JsonSerializer _newtonsoftSerializer =
-            Newtonsoft.Json.JsonSerializer.CreateDefault();
-
         public TableData()
         {
         }
@@ -26,10 +22,7 @@ namespace Kustox.BlobStorageState.DataObjects
             ColumnTypes = result.Columns
                 .Select(c => c.ColumnType.FullName)
                 .ToImmutableArray();
-            Data = result.Data
-                .Select(r => r.Select(o => AlignTypeToNewtonsoftFriendly(o)).ToImmutableArray())
-                .Cast<IImmutableList<object>>()
-                .ToImmutableArray();
+            Data = result.Data;
         }
 
         public bool IsScalar { get; set; }
@@ -39,33 +32,5 @@ namespace Kustox.BlobStorageState.DataObjects
         public IImmutableList<string>? ColumnTypes { get; set; }
 
         public IImmutableList<IImmutableList<object>>? Data { get; set; }
-
-        private static object AlignTypeToNewtonsoftFriendly(object obj)
-        {
-            if (obj is JsonValue)
-            {
-                var text = JsonSerializer.Serialize(obj);
-                var textReader = new StringReader(text);
-                var jsonTextReader = new Newtonsoft.Json.JsonTextReader(textReader);
-                var textObj = _newtonsoftSerializer.Deserialize<Newtonsoft.Json.Linq.JObject>(
-                    jsonTextReader);
-
-                return textObj!;
-            }
-            else if (obj is JsonArray)
-            {
-                var text = JsonSerializer.Serialize(obj);
-                var textReader = new StringReader(text);
-                var jsonTextReader = new Newtonsoft.Json.JsonTextReader(textReader);
-                var textObj = _newtonsoftSerializer.Deserialize<Newtonsoft.Json.Linq.JArray>(
-                    jsonTextReader);
-
-                return textObj!;
-            }
-            else
-            {
-                return obj;
-            }
-        }
     }
 }
