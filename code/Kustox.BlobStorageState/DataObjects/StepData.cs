2a13b2a625a679d037d8613fe148e9e237b7aa74
vplauzon <vincent_lauzon@yahoo.com.au>
2023-08-29 19:39:51 -0400

    .

--- code/Kustox.BlobStorageState/DataObjects/StepData.cs
+++ code/Kustox.BlobStorageState/DataObjects/StepData.cs
@@ -93,6 +93,8 @@ namespace Kustox.BlobStorageState.DataObjects
                     Script,
                     Breadcrumb,
                     State,
+                    null,
+                    null,
                     Timestamp);
             }
         }
