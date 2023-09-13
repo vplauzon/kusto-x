eb6cb77c884d81cd7db84953e4366092f22e66cf
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-13 12:09:38 -0400

    Add RunProcedureCommandRunner

---
+++ code/Kustox.Runtime/Commands/RunProcedureCommandRunner.cs
@@ -0,0 +1,20 @@
+﻿using Kustox.Compiler;
+using Kustox.Runtime.State.RunStep;
+
+namespace Kustox.Runtime.Commands
+{
+    internal class RunProcedureCommandRunner : CommandRunnerBase
+    {
+        public RunProcedureCommandRunner(ConnectionProvider connectionProvider)
+            : base(connectionProvider)
+        {
+        }
+
+        public override Task<TableResult> RunCommandAsync(
+            CommandDeclaration command,
+            CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
