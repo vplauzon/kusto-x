f1bf81ce55d4ae4080e8ce7a8911b001a7eed200
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-26 14:39:46 -0400

    .

--- code/Kustox.IntegratedTests/Commands/ShowProcRunsTest.cs
+++ code/Kustox.IntegratedTests/Commands/ShowProcRunsTest.cs
@@ -2,6 +2,7 @@ using Kustox.Compiler;
 using Kustox.Runtime;
 using Kustox.Runtime.State;
 using System.Collections.Immutable;
+using System.Text.Json;
 
 namespace Kustox.IntegratedTests.Commands
 {
@@ -26,6 +27,18 @@ namespace Kustox.IntegratedTests.Commands
             Assert.NotNull(result);
             Assert.False(result.IsScalar);
             Assert.Empty(result.Data);
+        }
+
+        [Fact]
+        public async Task WithQuery()
+        {
+            var script = ".show procedure runs | project A='123'";
+            var result = await RunStatementAsync(script);
+
+            Assert.NotNull(result);
+            Assert.False(result.IsScalar);
+            Assert.NotEmpty(result.Data);
+            Assert.Equal("123", result.Data[0][0]);
         }
     }
 }
