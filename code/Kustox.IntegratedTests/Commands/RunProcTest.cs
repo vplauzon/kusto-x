923d0d31d0beaddb925393c07f62f1f53baa1ce8
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-25 20:43:13 -0400

    Create commands sub folder

--- code/Kustox.IntegratedTests/Commands/RunProcTest.cs
+++ code/Kustox.IntegratedTests/Commands/RunProcTest.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Kustox.IntegratedTests
+namespace Kustox.IntegratedTests.Commands
 {
     public class RunProcTest : TestBase
     {
