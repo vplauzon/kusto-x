704a36a929a280239cbd3922b098b06e2febc962
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-28 12:02:48 -0400

    Add test

--- code/Kustox.IntegratedTests/Commands/ShowProcRunsStepsResultTest.cs
+++ code/Kustox.IntegratedTests/Commands/ShowProcRunsStepsResultTest.cs
@@ -18,18 +18,19 @@ namespace Kustox.IntegratedTests.Commands
             Assert.False(result.IsScalar);
         }
 
-        //[Fact]
-        //public async Task WithQuery()
-        //{
-        //    var script = ".show procedure runs 'abc' steps [1,2,3] result | project A='123'";
-        //    var result = await RunStatementAsync(script);
+        [Fact]
+        public async Task WithQuery()
+        {
+            var script = ".show procedure runs 'abc' steps [1,2,3] result | project A='123'";
+            var result = await RunStatementAsync(script);
 
-        //    Assert.NotNull(result);
-        //    Assert.False(result.IsScalar);
-        //    Assert.Empty(result.Data);
-        //    Assert.Single(result.Columns);
-        //    Assert.Equal("A", result.Columns[0].ColumnName);
-        //    Assert.Equal(typeof(string), result.Columns[0].ColumnType);
-        //}
+            Assert.NotNull(result);
+            Assert.False(result.IsScalar);
+            Assert.Single(result.Data);
+            Assert.Single(result.Columns);
+            Assert.Equal("A", result.Columns[0].ColumnName);
+            Assert.Equal(typeof(string), result.Columns[0].ColumnType);
+            Assert.Equal("123", result.Data[0][0]);
+        }
     }
 }
