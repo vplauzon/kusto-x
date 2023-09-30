5921ef48d10e4c4f2379de42927db94c8d85dff8
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-30 17:14:20 -0400

    .

--- code/Kustox.KustoState/StreamingBuffer.cs
+++ code/Kustox.KustoState/StreamingBuffer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kusto.Language;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace Kustox.KustoState
 {
-    internal class StreamingBuffer
+    internal class StreamingBuffer<T>
     {
+        #region Inner Types
+        private record QueueItem(
+            byte[] buffer,
+            TaskCompletionSource completionSource);
+        #endregion
+
+        private readonly StreamingBufferOptions _options;
+        private readonly ConcurrentQueue<QueueItem> _queue = new ConcurrentQueue<QueueItem>();
+
+        public StreamingBuffer(StreamingBufferOptions? options = null)
+        {
+            _options = options == null
+                ? new StreamingBufferOptions()
+                : new StreamingBufferOptions(options);
+        }
+
+        public Task AppendRecords(
+            IEnumerable<T> records,
+            CancellationToken ct)
+        {
+            QueueRecords(records);
+
+            throw new NotImplementedException();
+        }
+
+        private void QueueRecords(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ code/Kustox.KustoState/StreamingBufferOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace Kustox.KustoState
+{
+    public class StreamingBufferOptions
+    {
+        #region Constructor
+        public StreamingBufferOptions()
+        {
+        }
+
+        public StreamingBufferOptions(StreamingBufferOptions original)
+        {
+        }
+        #endregion
+
+        /// <summary>
+        /// Interval to wait for items to queue before pushing them for streaming.
+        /// </summary>
+        public TimeSpan BatchInterval { get; set; } = TimeSpan.FromSeconds(0.1);
+
+        /// <summary>Interval to wait before retrying a throttled streaming ingestion.</summary>
+        public TimeSpan ThrottleRetryInterval { get; set; } = TimeSpan.FromSeconds(0.05);
+
+        /// <summary>Interval to wait before retrying to get elected.</summary>
+        public TimeSpan ElectionRetryInterval { get; set; } = TimeSpan.FromSeconds(0.1);
+    }
+}
