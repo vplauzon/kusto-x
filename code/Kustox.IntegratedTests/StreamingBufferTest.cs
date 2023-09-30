d099692ea939184bb37aa401edf5939e2e51bc82
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-30 18:29:24 -0400

    .

---
+++ code/Kustox.IntegratedTests/StreamingBufferTest.cs
@@ -0,0 +1,33 @@
+﻿using Kustox.Runtime.State;
+using Kustox.Runtime.State.Run;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kustox.IntegratedTests
+{
+    internal class StreamingBufferTest : TestBase
+    {
+        public async Task MultipleAppend()
+        {
+            var store = StorageHub.ProcedureRunStore;
+            var ct = new CancellationToken();
+            //  Run 10 appends in parallel
+            var tasks = Enumerable.Range(0, 10)
+                .Select(i => new ProcedureRun(
+                    $"test-{i}",
+                    ProcedureRunState.Completed,
+                    DateTime.Now))
+                .Select(run => Task.Run(async () =>
+                {
+                    await store.AppendRunAsync(new[] { run }, ct);
+                }))
+                .ToImmutableArray();
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
