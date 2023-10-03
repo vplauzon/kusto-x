2507eb6c0c625a0997244d362da922fac965c126
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-02 20:24:16 -0400

    Add test

--- code/Kustox.UnitTests/Commands/AppendTest.cs
+++ code/Kustox.UnitTests/Commands/AppendTest.cs
@@ -13,6 +13,23 @@ namespace Kustox.UnitTests.Commands
             Assert.NotNull(statement);
             Assert.NotNull(statement.Command);
             Assert.NotNull(statement.Command.AppendCommand);
+            Assert.Empty(statement.Command.AppendCommand.Properties);
+            Assert.Equal("MyTable", statement.Command.AppendCommand.TableName);
+            Assert.Equal("myVar", statement.Command.AppendCommand.CaptureId);
+        }
+
+        [Fact]
+        public void WithFolderProperty()
+        {
+            var script = @$".append MyTable with (folder=""myfolder"") <| myVar";
+            var statement = new KustoxCompiler().CompileStatement(script);
+
+            Assert.NotNull(statement);
+            Assert.NotNull(statement.Command);
+            Assert.NotNull(statement.Command.AppendCommand);
+            Assert.Single(statement.Command.AppendCommand.Properties);
+            Assert.Equal("folder", statement.Command.AppendCommand.Properties.First().Id);
+            Assert.Equal("myfolder", statement.Command.AppendCommand.Properties.First().String);
             Assert.Equal("MyTable", statement.Command.AppendCommand.TableName);
             Assert.Equal("myVar", statement.Command.AppendCommand.CaptureId);
         }
