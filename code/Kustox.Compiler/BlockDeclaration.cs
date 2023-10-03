174c996b9e9e37cf9e7e1a581059b9c5e7a32eb9
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-03 17:56:52 -0400

    Parse if

--- code/Kustox.Compiler/BlockDeclaration.cs
+++ code/Kustox.Compiler/BlockDeclaration.cs
@@ -13,6 +13,8 @@ namespace Kustox.Compiler
         public CommandDeclaration? Command { get; set; }
 
         public ForEachDeclaration? ForEach { get; set; }
+        
+        public IfDeclaration? If { get; set; }
 
         internal override void Validate()
         {
@@ -20,6 +22,7 @@ namespace Kustox.Compiler
             
             var capturableCount = (ForEach == null ? 0 : 1)
                 + (Command == null ? 0 : 1)
+                + (If == null ? 0 : 1)
                 + (Query == null ? 0 : 1);
 
             if (capturableCount != 1)
@@ -32,6 +35,7 @@ namespace Kustox.Compiler
             Query?.Validate();
             Command?.Validate();
             ForEach?.Validate();
+            If?.Validate();
         }
     }
 }
---
+++ code/Kustox.UnitTests/IfTest.cs
@@ -0,0 +1,31 @@
+using Kustox.Compiler;
+
+namespace Kustox.UnitTests
+{
+    public class IfTest
+    {
+        [Fact]
+        public void EmptyThen()
+        {
+            var script = @".run proc <| {
+    @if a {
+    }
+}";
+            var statement = new KustoxCompiler().CompileStatement(script);
+
+            Assert.NotNull(statement);
+            Assert.NotNull(statement.Command);
+            Assert.NotNull(statement.Command.RunProcedureCommand);
+            Assert.Single(statement.Command.RunProcedureCommand.RootSequence.Blocks);
+
+            var blocks = statement.Command.RunProcedureCommand.RootSequence.Blocks;
+
+            //  If
+            Assert.Null(blocks.First().Capture);
+            Assert.NotNull(blocks.First().If);
+            Assert.Equal("a", blocks.First().If!.Condition);
+            Assert.Empty(blocks.First().If!.ThenSequence.Blocks);
+            Assert.Null(blocks.First().If!.ElseSequence);
+        }
+    }
+}
