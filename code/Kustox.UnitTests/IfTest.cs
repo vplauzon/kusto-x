1ec3604ae6065c4af85221f0f9cbded4496489c6
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-04 08:59:48 -0400

    Add test

--- code/Kustox.UnitTests/IfTest.cs
+++ code/Kustox.UnitTests/IfTest.cs
@@ -27,5 +27,33 @@ namespace Kustox.UnitTests
             Assert.Empty(blocks.First().If!.ThenSequence.Blocks);
             Assert.Null(blocks.First().If!.ElseSequence);
         }
+
+        [Fact]
+        public void TwoStatementsThen()
+        {
+            var script = @".run proc <| {
+    @if a {
+        .create table A(Id:string)
+
+        .show version
+
+    }
+}";
+            var statement = new KustoxCompiler().CompileStatement(script);
+
+            Assert.NotNull(statement);
+            Assert.NotNull(statement.Command);
+            Assert.NotNull(statement.Command.RunProcedureCommand);
+            Assert.Single(statement.Command.RunProcedureCommand.RootSequence.Blocks);
+
+            var blocks = statement.Command.RunProcedureCommand.RootSequence.Blocks;
+
+            //  If
+            Assert.Null(blocks.First().Capture);
+            Assert.NotNull(blocks.First().If);
+            Assert.Equal("a", blocks.First().If!.Condition);
+            Assert.Equal(2, blocks.First().If!.ThenSequence.Blocks.Count);
+            Assert.Null(blocks.First().If!.ElseSequence);
+        }
     }
 }
