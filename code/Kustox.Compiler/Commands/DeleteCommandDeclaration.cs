5ab1d8898858c3e1e401343af79f9813de1cc7c4
vplauzon <vincent_lauzon@yahoo.com.au>
2023-10-04 12:32:18 -0400

    .

---
+++ code/Kustox.Compiler/Commands/DeleteCommandDeclaration.cs
@@ -0,0 +1,26 @@
+﻿using Kusto.Language;
+
+namespace Kustox.Compiler.Commands
+{
+    public class DeleteCommandDeclaration : DeclarationBase
+    {
+        public string TableName { get; set; } = string.Empty;
+
+        public QueryDeclaration? Query { get; set; }
+
+        internal override void Validate()
+        {
+            base.Validate();
+
+            if (string.IsNullOrWhiteSpace(TableName))
+            {
+                throw new InvalidDataException($"No '{nameof(TableName)}'");
+            }
+            if (Query == null)
+            {
+                throw new InvalidDataException($"No '{nameof(Query)}'");
+            }
+            Query.Validate();
+        }
+    }
+}
